70f1270fc41ea0f37eb8a3747e0cab10f2469fe7
yun-mh <minhoyun.dev@gmail.com>
2019-10-09 23:54:41 +0900

    Add models

--- rooms/models.py
+++ rooms/models.py
@@ -4,6 +4,46 @@ from django_countries.fields import CountryField
 from users import models as user_models
 
 # Create your models here.
+class AbstractItem(core_models.TimeStampedModel):
+
+    """ Abstract Item """
+
+    name = models.CharField(max_length=80)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class RoomType(AbstractItem):
+    
+    """ RoomType Model Definition """
+
+    pass
+
+class Amenity(AbstractItem):
+
+    """ Amenity Model Definition """
+
+    pass
+
+
+class Facility(AbstractItem):
+
+    """ Facility Model Definition """
+
+    pass
+
+
+class HouseRule(AbstractItem):
+
+    """ HouseRule Model Definition """
+
+    pass
+
+
 class Room(core_models.TimeStampedModel):
     
     """ Room Model Definition """
@@ -22,4 +62,10 @@ class Room(core_models.TimeStampedModel):
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
     host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField(Amenity)
+    facilities = models.ManyToManyField(Facility)
+    house_rules = models.ManyToManyField(HouseRule)
 
+    def __str__(self):
+        return self.name
