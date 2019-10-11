aab1acf5192ac97442e1e09ddb243bd38a67fdf7
yun-mh <minhoyun.dev@gmail.com>
2019-10-12 00:59:05 +0900

    Add related name for manipulating ORM

--- rooms/admin.py
+++ rooms/admin.py
@@ -7,7 +7,10 @@ class ItemAdmin(admin.ModelAdmin):
 
     """ Item Admin Definition """
 
-    pass
+    list_display = ("name", "used_by")
+
+    def used_by(self, obj):
+        return obj.rooms.count()
 
 
 @admin.register(models.Room)
@@ -46,6 +49,7 @@ class RoomAdmin(admin.ModelAdmin):
         "check_out",
         "instant_book",
         "count_amenities",
+        "count_photos",
     )
 
     list_filter = (
@@ -68,6 +72,9 @@ class RoomAdmin(admin.ModelAdmin):
         return obj.amenities.count()
 
     # count_amenities.short_description = "hello!"
+
+    def count_photos(self, obj):
+        return obj.photos.count()
 
 
 @admin.register(models.Photo)
