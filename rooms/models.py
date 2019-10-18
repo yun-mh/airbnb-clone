2aac3e169d6efb1f2c166c70902889d173affe4b
yun-mh <minhoyun.dev@gmail.com>
2019-10-18 22:44:38 +0900

    Structuring detail views and search views

--- rooms/models.py
+++ rooms/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from core import models as core_models
 from django_countries.fields import CountryField
 from users import models as user_models
@@ -95,6 +96,9 @@ class Room(core_models.TimeStampedModel):
     def save(self, *args, **kwargs):
         self.city = str.capitalize(self.city)
         super().save(*args, **kwargs)  # Call the real save() method
+
+    def get_absolute_url(self):
+        return reverse("rooms:detail", kwargs={'pk': self.pk})
 
     def total_rating(self):
         all_reviews = self.reviews.all()
--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,6 @@
-from django.utils import timezone
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.shortcuts import render
+from django_countries import countries
 from . import models
 
 # Create your views here.
@@ -13,8 +14,32 @@ class HomeView(ListView):
     ordering = "created"
     context_object_name = "rooms"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        now = timezone.now()
-        context["now"] = now
-        return context
+
+class RoomDetail(DetailView):
+
+    """ RoomDetail Definition """
+
+    model = models.Room
+
+
+def search(request):
+    city = request.GET.get("city", "Anywhere")
+    city = str.capitalize(city)
+    country = request.GET.get("country", "KR")
+    room_type = int(request.GET.get("room_type", 0))
+    room_types = models.RoomType.objects.all()
+
+    form = {"city": city, "selected_room_type": room_type, "selected_country": country}
+
+    choices = {"countries": countries, "room_types": room_types}
+
+    return render(request, "rooms/search.html", {**form, **choices})
+
+
+# FBV of detail view
+# def room_detail(request, pk):
+#     try:
+#         room = models.Room.objects.get(pk=pk)
+#         return render(request, "rooms/detail.html", {"room": room})
+#     except models.Room.DoesNotExist:
+#         raise Http404()
