7e37295a9a0dbf9fb7cf6c368f1b05ed8c9aff62
yun-mh <minhoyun.dev@gmail.com>
2019-11-04 00:04:08 +0900

    Implement Kakao login feature

--- users/views.py
+++ users/views.py
@@ -5,7 +5,6 @@ from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import authenticate, login, logout
 from django.core.files.base import ContentFile
-from django.contrib.auth.forms import UserCreationForm
 from . import forms, models
 
 # Create your views here.
@@ -47,7 +46,7 @@ def log_out(request):
 class SignUpView(FormView):
 
     template_name = "users/signup.html"
-    form_class = forms.UserCreationForm
+    form_class = forms.SignUpForm
     success_url = reverse_lazy("core:home")
     initial = {
         "first_name": "Minho",
