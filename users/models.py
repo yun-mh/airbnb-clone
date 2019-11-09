cf0f64d95e50509cccac82642cf91156299a0a35
yun-mh <minhoyun.dev@gmail.com>
2019-11-09 23:19:07 +0900

    Create user profile page

--- users/models.py
+++ users/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 from django.core.mail import send_mail
 from django.utils.html import strip_tags
 from django.template.loader import render_to_string
+from django.shortcuts import reverse
 from django.db import models
 
 # Create your models here.
@@ -57,6 +58,9 @@ class User(AbstractUser):
     login_method = models.CharField(
         max_length=50, choices=LOGIN_CHOICES, default=LOGIN_EMAIL
     )
+
+    def get_absolute_url(self):
+        return reverse("users:profile", kwargs={"pk": self.pk})
 
     def verify_email(self):
         if self.email_verified is False:
