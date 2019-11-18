a0bbff4aadc6a362896d38ffe11a53f511361657
yun-mh <minhoyun.dev@gmail.com>
2019-11-18 22:07:22 +0900

    Put calendars into room detail page

--- rooms/models.py
+++ rooms/models.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from core import models as core_models
 from django_countries.fields import CountryField
 from users import models as user_models
+from cal import Calendar
 
 # Create your models here.
 class AbstractItem(core_models.TimeStampedModel):
@@ -120,3 +121,7 @@ class Room(core_models.TimeStampedModel):
         photos = self.photos.all()[1:5]
         return photos
 
+    def get_calendars(self):
+        this_month = Calendar(2019, 11)
+        next_month = Calendar(2019, 12)
+        return [this_month, next_month]
