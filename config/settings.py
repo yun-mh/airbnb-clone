f42073bf39b1a2fe0dcc0c78ca2aba627b017eb1
yun-mh <minhoyun.dev@gmail.com>
2020-02-23 14:58:59 +0900

    Updated

--- config/settings.py
+++ config/settings.py
@@ -46,7 +46,7 @@ DJANGO_APPS = [
     "django.contrib.staticfiles",
 ]
 
-THIRD_PARTY_APPS = ["django_countries", "django_seed"]
+THIRD_PARTY_APPS = ["django_countries", "django_seed", "storages"]
 
 PROJECT_APPS = [
     "core.apps.CoreConfig",
@@ -175,8 +175,18 @@ LOGIN_URL = "/users/login"
 
 LOCALE_PATHS = [os.path.join(BASE_DIR, "locale")]
 
+
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = "airbnb-clone-minhoyundev"
+AWS_AUTO_CREATE_BUCKET = True
+AWS_BUCKET_ACL = "public_read"
+
 # Sentry
 if not DEBUG:
+
     sentry_sdk.init(
         dsn=os.environ.get("SENTRY_URL"),
         integrations=[DjangoIntegration()],
@@ -184,4 +194,3 @@ if not DEBUG:
         # django.contrib.auth) you may enable sending PII data.
         send_default_pii=True,
     )
-
